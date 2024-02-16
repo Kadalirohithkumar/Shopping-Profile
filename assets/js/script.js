d48f963eaee186aa268c70637051512c5becee68
Kadalirohithkumar <96775264+Kadalirohithkumar@users.noreply.github.com>
2024-02-16 23:36:22 +0530

    modified:   assets/css/style.css 	modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,48 @@
+// // Navigation
+// // Responsive Toggle Navigation =============================================
+// let menuIcon = document.querySelector('.menuIcon');
+// let nav = document.querySelector('.overlay-menu');
+
+// menuIcon.addEventListener('click', () => {
+//     // Use getComputedStyle to get the applied style and check the transform value
+//     const computedStyle = window.getComputedStyle(nav);
+//     const transformValue = computedStyle.getPropertyValue('transform');
+
+//     if (transformValue === 'matrix(1, 0, 0, 1, 0, 0)') {
+//         nav.style.transform = 'translateX(0%)';
+//         nav.style.transition = 'transform 0.2s ease-out';
+//     } else {
+//         nav.style.transform = 'translateX(-100%)';
+//         nav.style.transition = 'transform 0.2s ease-out';
+//     }
+// });
+
+// // Toggle Menu Icon ========================================
+// let toggleIcon = document.querySelector('.menuIcon');
+
+// toggleIcon.addEventListener('click', () => {
+//     toggleIcon.classList.toggle('toggle');
+// });
+
+// function changeBg() {
+//     var indi = document.getElementById("indi");
+//     var scrollValue = window.scrollY;
+//     if (scrollValue < 400) {
+//         indi.style.display = "none";
+//     } else if (scrollValue > 400) {
+//         indi.style.display = "block";
+//     }
+// }
+// changeBg();
+
+// window.addEventListener("scroll", changeBg);
+
+// function toggleMobileMenu() {
+//     var mobileMenu = document.getElementById("menu");
+//     mobileMenu.style.display = (mobileMenu.style.display === "block") ? "none" : "block";
+// }
 
 
 
-    /*============================================================================
-                                    Évelym S.
-                    https://www.linkedin.com/in/evelym-santos/
-  ============================================================================*/
 
-    // Navigation
-        // Responsive Toggle Navigation =============================================
-        let menuIcon = document.querySelector('.menuIcon');
-        let nav = document.querySelector('.overlay-menu');
-
-        menuIcon.addEventListener('click', () => {
-            if (nav.style.transform != 'translateX(0%)') {
-                nav.style.transform = 'translateX(0%)';
-                nav.style.transition = 'transform 0.2s ease-out';
-            } else { 
-                nav.style.transform = 'translateX(-100%)';
-                nav.style.transition = 'transform 0.2s ease-out';
-            }
-        });
-
-
-        // Toggle Menu Icon ========================================
-        let toggleIcon = document.querySelector('.menuIcon');
-
-        toggleIcon.addEventListener('click', () => {
-            if (toggleIcon.className != 'menuIcon toggle') {
-                toggleIcon.className += ' toggle';
-            } else {
-                toggleIcon.className = 'menuIcon';
-            }
-        });
-
-
-        function changeBg() {
-          var indi = document.getElementById("indi");
-          var scrollValue = window.scrollY;
-          if (scrollValue < 400) {
-            indi.style.display = "none";
-          } else if (scrollValue > 400) {
-            indi.style.display = "block";
-          }
-        }
-        changeBg();
-
-        window.addEventListener("scroll", changeBg);
+// // Fetching
